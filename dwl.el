cc5c66ab6674c1f10ce44190a2dd70f5d116daab
Nicola Ferru aka NFVblog <ask.nfvblog@outlook.it>
2023-12-29 22:29:05 +0100

    init

---
+++ dwl.el
@@ -0,0 +1,11 @@
+;;; dwl.el --- Comunication with the dwl window manager -*- lexical-binding: t; coding: utf-8 -*-
+
+;; Copyright (c) 2023-2024 Nicola Ferru <ask dot nfvblog at outlook dot it>
+
+;; Author: Nicola Ferru <ask dot nfvblog at outlook dot it>
+;; Maintainer: Nicola Ferru <ask dot nfvblog at outlook dot it>
+;;
+;; Keywords: frames
+;; Homepage: https://github.com/nf02/dwl.el
+;; Version: 0.7
+;; Package-Requires: ((emacs "28.1"))
